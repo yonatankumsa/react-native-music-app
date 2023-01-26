cd2f931ebb701909f20b139b0cf1658a700c0cec
yonatankumsa <guitaristyona@gmail.com>
2023-01-25 20:22:15 -0500

    remove playlist data

--- data/index.js
+++ data/index.js
@@ -99,11 +99,6 @@ export const playlists = [
     items: [1, 2, 3, 4, 5],
   },
   {
-    id: 2,
-    title: 'Road',
-    items: [1, 6, 7],
-  },
-  {
     id: 3,
     title: 'Rock',
     items: [8, 9],
